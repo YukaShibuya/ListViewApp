bb92ed8ebceca94b971179b106cf7d6283f1723c
YukaShibuya <pfshibuyu@gmail.com>
2020-09-06 15:58:10 +0900

    Initial Commit

---
+++ ListViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewApp
+//
+//  Created by 渋谷柚花 on 2020/09/06.
+//  Copyright © 2020 渋谷柚花. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
